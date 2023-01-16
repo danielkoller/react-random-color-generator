ce93e3fa1921606fe918b40f3737ad71d2784660
Daniel Koller <daniel-koller@gmx.at>
2023-01-16 16:52:26 +0100

    Format with prettier and fix reportWebVitals

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, { useState } from 'react';
 import randomcolor from 'randomcolor';
+import React, { useState } from 'react';
 
 export default function RandomColorApp() {
-const [color, setColor] = useState(randomcolor());
+  const [color, setColor] = useState(randomcolor());
 
-return (
-<div>
-  <h1>Random Color Generator</h1>
-<p>The hex code for the random color is: <b>{color}</b></p>
-<div style={{ backgroundColor: color, width: "100px", height: "100px", marginBottom: "10px", marginLeft: "10px"}} />
-<button onClick={() => setColor(randomcolor())}>
-Generate
-</button>
-</div>
-);
+  return (
+    <div>
+      <h1>Random Color Generator</h1>
+      <p>
+        The hex code for the random color is: <b>{color}</b>
+      </p>
+      <div
+        style={{
+          backgroundColor: color,
+          width: '100px',
+          height: '100px',
+          marginBottom: '10px',
+          marginLeft: '10px',
+        }}
+      />
+      <button onClick={() => setColor(randomcolor())}>Generate</button>
+    </div>
+  );
 }
