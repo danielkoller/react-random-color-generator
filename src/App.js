07c4c7500cb5e08d5f82f7e1ac49c71ad81ae6f8
Daniel Koller <daniel-koller@gmx.at>
2023-01-16 16:28:51 +0100

    Add basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import randomcolor from 'randomcolor';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function RandomColorApp() {
+const [color, setColor] = useState(randomcolor());
+
+return (
+<div>
+  <h1>Random Color Generator</h1>
+<p>The hex code for the random color is: <b>{color}</b></p>
+<div style={{ backgroundColor: color, width: "100px", height: "100px", marginBottom: "10px", marginLeft: "10px"}} />
+<button onClick={() => setColor(randomcolor())}>
+Generate
+</button>
+</div>
+);
 }
-
-export default App;
